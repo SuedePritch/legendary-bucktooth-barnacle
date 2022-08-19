a42e6a5a4003fa755957b2dd18975f080b27cf2c
James Pritchard <james.edward.pritchard@gmail.com>
2022-08-19 16:16:56 -0400

    updated resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -9,58 +9,46 @@ function Resume() {
                     <a href="./JamesPritchardResume.pdf" alt="Resume" download>Download Resume</a>
                 </button>
 <h1>James Pritchard</h1>
-Phone: 1-250-617-0245 <br />
-Thunder Bay, Ontario, Canada<br />
-<a href="mailto:james.edward.pritchard@gmail.com" id="email-link">james.edward.pritchard@gmail.com</a>
+<ul>
+    <li>Thunder Bay, Ontario, Canada | <a href="tel:1-250-617-0145">Phone 250-617-0145</a> | <a href="mailto:james.edward.pritchard@gmail.com">Email: james.edward.pritchard@gmail.com</a></li>
+    <li><a href="https://github.com/SuedePritch">GitHub: https://bit.ly/3ps8ulO</a> | <a href="https://www.linkedin.com/in/james-pritchard-a2b06b233/">LinkedIn: https://bit.ly/3CdRd7w </a>| <a href="https://suedepritch.github.io/legendary-bucktooth-barnacle/">Portfolio: https://bit.ly/3AsMAF6</a></li>
+</ul>
 
-<h2>Summary</h2>
-Full-stack developer with a certificate from the University of Toronto full-stack development program. Specializing in the MERN stack with extensive knowledge of blockchain and web3 technologies. Excel at producing clean, readable, well-commented code while consistently striving to improve technical skills. Intensely curious about technology and eager to implement cutting-edge and legacy technologies.
+
+<h2>Professional Summary</h2>
+<p>Full-stack developer with a certificate from the University of Toronto full-stack development program. Proven problem solver specializing in the MERN stack with extensive knowledge of blockchain and web3 technologies. Life-long learner that excels at producing clean, readable, well-commented code while consistently striving to improve technical skills. Intensely curious about technology and eager to implement cutting-edge solutions.</p>
 
 <h2>Technical Skills</h2>
-<ul>
-    <li>JavaScript/TypeScript</li>
-    <li>React/Angular</li>
-    <li>HTML/CSS/SCSS/SASS</li>
-    <li>Scalable Vector Graphics</li>
-    <li>NodeJS</li>
-    <li>MongoDB/MySQL</li>
-    <li>Solidity</li>
-    <li>Truffle/Ganache</li>
-    <li>Remix/VSCode</li>
-    <li>Git/GitHub</li>
-</ul>
+<p className='tech-skill-list'>JavaScript | TypeScript | React | Angular | HTML | CSS | SCSS | SASS | Scalable Vector Graphics | NodeJS | MongoDB | MySQL | Solidity | Truffle | Ganache | Remix | VSCode | Git | GitHub</p>
 
-<h2>Education</h2>
-University of Toronto Full Stack Developer Certificate <br />
-MongoDB University<br />
-Dapp University<br />
-Blockgeeks<br />
-Society of Cable Engineers: Maintenance Technology Level 2, Network Testing and Maintenance Level 1<br />
-Fanshawe College Machinist Apprenticeship
+<h2>Relevant Professional Experience</h2>
+<div>
+    <h3>Freelance Web Developer</h3>
+    <p>2022 - Present</p>
+    <p>Plan and develop software solutions and web applications, supporting and maintaining websites and digital products for various small companies. Creates solutions for identified problems or bugs.Develops and validates test routines to ensure the quality of the external and internal interface.</p>
+</div>
 
-<h2>Experience</h2>
-<h3>
-    Shaw Cable Systems Ltd., Fort St John BC <br />
-    System Technician <br />
-    2019-2021
-</h3>
-<ul>
-    <li>Maintained Shaw’s internet and cable infrastructure to ensure a positive client experience.   	
-    </li>
-    <li>Installed services to business and residential customers including cable, internet, phone, and security systems.</li>
-    <li>Facilitated significant upgrades to local infrastructure that increased local systems from 750/860Mhz to 1.5 GHz allowing Orthogonal Frequency Division Multiplexing.</li>
-</ul>
-<h3>
-    Spectrum Resource Group Inc & Windfirm & Zanzibar & Reclaimit <br />
-    Foreman, OFA Level 3, Silviculture Worker, Timber Cruiser, Faller, Brusher <br />
-    2009-2018
-</h3>
-<ul>
-    <li>Navigated quality standards set by the Ministry of Forests, Lands, Natural Resources.</li>
-    <li>Trained workers in the field while providing information on ever-changing safety guidelines and precautions, such as wildlife or dangerous trees.</li>
-    <li>Acted as primary Occupational First Aid Level 3 attendant establishing care for remote worksites with large workforces.</li>
-    <li>Managed scheduled projects ensuring strict compliance with all applicable deadlines and budgets. </li>
-</ul>
+<h2>Additional Professional Experience</h2>
+<div>
+    <h3>Shaw Cable Systems Ltd.<br /> Fort St. John, BC</h3>
+    <p>2019 - 2021</p>
+    <b>System Technician</b>
+    <p>
+        Maintained internet and cable infrastructure to ensure 99.99% uptime for 16000+ customers.
+        Installed services to business and residential customers including cable, internet, phone, and security systems with a 4.8/5 customer feedback score.
+        Facilitated upgrades to local infrastructure that increased systems from 860Mhz to 1.5 GHz allowing Orthogonal Frequency Division Multiplexing increasing upload/downspeeds from 600mbps to 1.5gbps.
+    </p>
+    <h3>Windfirm Resources<br /> Smithers, BC</h3>
+    <p>2009 - 2019</p>
+    <b>Foreman | Faller | Silviculture |  OFA Level 3</b>
+    <p>
+        Planted over 500,000 seedlings with my highest production day being 4735.
+        Trained workers on proper technique, chainsaw safety, tree identification, orienteering, and safety systems with perfect quality and no major incidents.
+        Served as the primary Occupational First Aid Level 3 attendant establishing care for remote worksites.
+        Managed scheduled projects ensuring strict compliance with all applicable deadlines and budgets.
+    </p>
+</div>
+
     </div>
     )
 }
